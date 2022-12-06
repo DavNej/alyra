83fb368df4ee1021af825ca2b3236cf42ed7a33d
DavNej <david.nejar@gmail.com>
2022-12-06 16:03:35 +0100

    whitelist add authorized function

--- solidity-basics/1206_Whitelist.sol
+++ solidity-basics/1206_Whitelist.sol
@@ -5,4 +5,9 @@ contract Whitelist {
     event Authorized(address _address);
 
     mapping(address => bool) whitelist;
+
+    function authorize(address _address) external {
+        whitelist[_address] = true;
+        emit Authorized(_address);
+    }
 }
