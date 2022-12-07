aea7e856cbe04107f962b67af92b9b1a9f123190
DavNej <david.nejar@gmail.com>
2022-12-07 07:43:19 +0100

    whitelist add modifier

--- solidity-basics/1205_PiggyBank.sol
+++ solidity-basics/1205_PiggyBank.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-// Wallet online
+// Online Wallet
 contract PiggyBank {
     struct Wallet {
         uint256 balance;
--- solidity-basics/1206_Whitelist.sol
+++ solidity-basics/1206_Whitelist.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+error Whitelist__NotAuthorized();
+
 contract Whitelist {
     event Authorized(address _address);
-
     mapping(address => bool) whitelist;
 
-    function authorize(address _address) external {
+    constructor() {
+        whitelist[msg.sender] = true;
+    }
+
+    modifier senderIsInWhitelist() {
+        // require(whitelist[msg.sender], "You are not authorized");
+        // OR
+        if (!whitelist[msg.sender]) {
+            revert Whitelist__NotAuthorized();
+        }
+        _;
+    }
+
+    function authorize(address _address) external senderIsInWhitelist {
         whitelist[_address] = true;
         emit Authorized(_address);
     }
