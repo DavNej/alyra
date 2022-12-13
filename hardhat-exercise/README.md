41e246335277d116392726f2eca54d9c75a9bdc6
DavNej <david.nejar@gmail.com>
2022-12-13 11:31:09 +0100

    hardhat-exercise config

--- hardhat-exercise/README.md
+++ hardhat-exercise/README.md
@@ -11,3 +11,12 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+
+## EXERCICE
+
+* Créer un contrat intelligent qui permet de dire "Hello" + prenom grâce à une function `sayHello()`
+* Le prénom est passé directement en argument du constructeur
+* Créer un setter et un getter pour le prénom
+* Utiliser la librairie "Ownable" de OpenZeppelin pour le setter et ajouter cette librairie au projet ("yarn add ? :o")
+* Déployer sur Goerli et Vérifier le contrat sur Etherscan
+* Utiliser Etherscan pour "jouer" avec le contrat
--- hardhat-exercise/hardhat.config.js
+++ hardhat-exercise/hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-etherscan')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+
+const GOERLI_RPC = process.env.INFURA_GOERLI || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETERSCAN_API_KEY = process.env.ETERSCAN_API_KEY || ''
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETERSCAN_API_KEY,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+      },
+    ],
+  },
+}
