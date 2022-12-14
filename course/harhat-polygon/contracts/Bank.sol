e1f3b780cb60542bb7a75454b40ef4e9e4bb4cec
DavNej <david.nejar@gmail.com>
2022-12-14 10:22:53 +0100

    hardhat-polygon contract verification

--- course/harhat-polygon/contracts/Bank.sol
+++ course/harhat-polygon/contracts/Bank.sol
@@ -5,10 +5,12 @@ contract Bank {
     mapping(address => uint256) balances;
 
     function deposit() external payable {
+        require(msg.value >= 100, "Minimum value to deposit is 100 wei");
         balances[msg.sender] += msg.value;
     }
 
     function withdraw() external {
+        require(balances[msg.sender] > 0, "No funds to withdraw");
         (bool sent, ) = msg.sender.call{value: balances[msg.sender]}("");
         require(sent, "Withdrawal failed");
     }
--- course/harhat-polygon/hardhat.config.js
+++ course/harhat-polygon/hardhat.config.js
@@ -1,10 +1,11 @@
 require('@nomicfoundation/hardhat-toolbox')
-// require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-etherscan')
 
 require('dotenv').config()
 
 const MUMBAI_RPC = process.env.ALCHEMY_MUMBAI || ''
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || ''
 
 module.exports = {
   defaultNetwork: 'hardhat',
@@ -13,10 +14,15 @@ module.exports = {
       url: 'http://127.0.0.1:8545',
       chainId: 31337,
     },
-    polygon: {
+    polygonMumbai: {
       url: MUMBAI_RPC,
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 80001,
+    },
+  },
+  polygonscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_API_KEY,
     },
   },
   solidity: {
