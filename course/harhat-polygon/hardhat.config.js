ca343a1da73a20acccbbd3432fa51af25e803fc9
DavNej <david.nejar@gmail.com>
2022-12-14 09:52:28 +0100

    hardhat-polygon config

--- course/harhat-polygon/hardhat.config.js
+++ course/harhat-polygon/hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-etherscan')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+
+const MUMBAI_RPC = process.env.ALCHEMY_MUMBAI || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+    polygon: {
+      url: MUMBAI_RPC,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 5,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+      },
+    ],
+  },
+}
