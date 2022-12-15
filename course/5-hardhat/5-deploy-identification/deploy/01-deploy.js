652ab39ce99444447f1286460eb0c3976d13fcc6
DavNej <david.nejar@gmail.com>
2022-12-15 11:14:21 +0100

    hardhat.identification | add first deploy script

---
+++ course/5-hardhat/5-deploy-identification/deploy/01-deploy.js
@@ -0,0 +1,27 @@
+const { network } = require('hardhat')
+const { developmentChains } = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  log('--------------------------------------')
+  const args = ['Jackson', 'Michael', 52]
+
+  const Identification = await deploy('Identification', {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+
+  //Verify the smart contract
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETERSCAN_API_KEY
+  ) {
+    log('Verifying...')
+    await verify(Identification.address, args)
+  }
+}
