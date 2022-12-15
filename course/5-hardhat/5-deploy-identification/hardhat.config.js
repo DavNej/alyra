f54b176e423f5f4eafab0ad854235511c3e4b496
DavNej <david.nejar@gmail.com>
2022-12-15 10:38:28 +0100

    hardhat.identification | config

--- course/5-hardhat/5-deploy-identification/hardhat.config.js
+++ course/5-hardhat/5-deploy-identification/hardhat.config.js
@@ -1,6 +1,43 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-etherscan')
+require('hardhat-deploy')
+
+const ALCHEMY_GOERLI = process.env.ALCHEMY_GOERLI || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETERSCAN_API_KEY = process.env.ETERSCAN_API_KEY || ''
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+    goerli: {
+      url: ALCHEMY_GOERLI,
+      chainId: 5,
+      accounts: [`0x${PRIVATE_KEY}`],
+      blockConfirmations: 6,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETERSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+      },
+    ],
+  },
+}
