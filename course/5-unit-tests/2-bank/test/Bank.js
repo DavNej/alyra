72d30efcd73b243e0eb263d4d07da54cc8f7d90c
DavNej <david.nejar@gmail.com>
2022-12-20 10:55:20 +0100

    unit-test.bank | test cases

--- course/5-unit-tests/2-bank/test/Bank.js
+++ course/5-unit-tests/2-bank/test/Bank.js
@@ -21,4 +21,57 @@ if (developmentChains.includes(network.name)) {
       assert.exists(Bank.deployed())
     })
   })
+
+  describe.only('deposit', async () => {
+    it('Revert when deposit is less than 100 wei', async () => {
+      const tx = await Bank.deposit(50)
+      expect(tx).to.be.revertedWith('Minimum deposit is 100 wei')
+      expect(tx).not.to.emit('etherDeposited')
+    })
+    it('Succeed when deposit is 100 wei', async () => {
+      // deposit(100)
+      // deposit success
+      // event emited
+    })
+    it('Succeed when deposit is greater than 100 wei', async () => {
+      // deposit(1000)
+      // deposit success
+      // event emited
+    })
+  })
+
+  describe('getBalanceAndLastDeposit', async () => {
+    it('Return balance and lastDeposit of account with NO funds', async () => {
+      // getBalanceAndLastDeposit()
+      // return
+    })
+
+    it('Return balance and lastDeposit of account with funds', async () => {
+      // getBalanceAndLastDeposit()
+      // return 0, 0
+    })
+  })
+
+  describe('withdraw', async () => {
+    it('Revert when amount is less than 100 wei', async () => {
+      // withdraw(50)
+      // reverts with message
+      // event not emited
+    })
+    it('Revert when amount is less than account balance', async () => {
+      // withdraw(1000)
+      // reverts with message
+      // event not emited
+    })
+    it('Succeed when withdraw is 100 wei', async () => {
+      // withdraw(100)
+      // withdraw success
+      // event emited
+    })
+    it('Succeed when withdraw is greater than 100 wei', async () => {
+      // withdraw(1000)
+      // withdraw success
+      // event emited
+    })
+  })
 }
