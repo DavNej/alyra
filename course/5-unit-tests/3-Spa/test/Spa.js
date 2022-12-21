60b8c411d3c275a58acbf69982f61e53c2f74594
DavNej <david.nejar@gmail.com>
2022-12-21 16:45:06 +0100

    unit-test.spa | test add function

--- course/5-unit-tests/3-Spa/test/Spa.js
+++ course/5-unit-tests/3-Spa/test/Spa.js
@@ -28,4 +28,29 @@ if (developmentChains.includes(network.name)) {
       assert.exists(spa.deployed())
     })
   })
+
+  describe('add', () => {
+    const animal = {
+      race: 'Dog',
+      size: 70,
+      age: 3,
+    }
+
+    it('Reverts if age is 0', async () => {
+      await expect(spa.add(animal.race, animal.size, 0)).to.be.revertedWith(
+        'Age must be greater than 0'
+      )
+    })
+
+    it('Reverts if size is 0', async () => {
+      await expect(spa.add(animal.race, 0, animal.age)).to.be.revertedWith(
+        'Size must be greater than 0'
+      )
+    })
+
+    it('Adds an animals', async () => {
+      const tx = await spa.add(animal.race, animal.size, animal.age)
+      expect(tx).to.emit('animalAdded').withArgs(0)
+    })
+  })
 }
