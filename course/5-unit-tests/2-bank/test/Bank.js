ae631b3a6fe45993d7176b3b4bc8345a3657a16d
DavNej <david.nejar@gmail.com>
2022-12-21 10:32:54 +0100

    unit-test.bank | refactor tests

--- course/5-unit-tests/2-bank/test/Bank.js
+++ course/5-unit-tests/2-bank/test/Bank.js
@@ -4,7 +4,7 @@ const { developmentChains } = require('../helper-hardhat-config')
 const { assert, expect } = require('chai')
 
 if (developmentChains.includes(network.name)) {
-  let Bank
+  let bank
   let _deployer
   let _user1
   let _user2
@@ -16,100 +16,120 @@ if (developmentChains.includes(network.name)) {
     _deployerInitialEtherBalance = _deployer.getBalance()
     _user1 = accounts[1]
     _user2 = accounts[2]
-    await deployments.fixture(['bank'])
-    Bank = await ethers.getContract('Bank')
   })
 
-  async function deposit(value, { account } = {}) {
-    const tx = account
-      ? await Bank.connect(account).deposit({ value })
-      : await Bank.deposit({ value })
+  beforeEach(async () => {
+    await deployments.fixture(['bank'])
+    bank = await ethers.getContract('Bank')
+  })
 
-    expect(tx).to.changeEtherBalance(
-      _deployer.address,
-      _deployerInitialEtherBalance - value
-    )
-    expect(tx).to.emit('etherDeposited').withArgs(_deployer.address, value)
-  }
-
-  describe('Units tests for smart contract Bank', () => {
+  describe('Contract deployment', () => {
     it('Deploy contract', async () => {
-      assert.exists(Bank.deployed())
+      assert.exists(bank.deployed())
+    })
+  })
+
+  describe('deposit', async () => {
+    it('Revert when deposit is less than 100 wei', async () => {
+      await expect(bank.deposit({ value: 50 })).to.be.revertedWith(
+        'Minimum deposit is 100 wei'
+      )
     })
 
-    describe('deposit', async () => {
-      it('Revert when deposit is less than 100 wei', async () => {
-        await expect(Bank.deposit({ value: 50 })).to.be.revertedWith(
-          'Minimum deposit is 100 wei'
-        )
-      })
+    it('Succeed when deposit is 100 wei', async () => {
+      const value = 100
+      const tx = await bank.deposit({ value })
 
-      it('Succeed when deposit is 100 wei', async () => {
-        await deposit(100)
-      })
+      expect(tx).to.changeEtherBalance(
+        _deployer.address,
+        _deployerInitialEtherBalance - value
+      )
 
-      it('Succeed when deposit is greater than 100 wei', async () => {
-        await deposit(1000)
-      })
+      expect(tx).to.emit('etherDeposited').withArgs(_deployer.address, value)
     })
 
-    describe('getBalanceAndLastDeposit', async () => {
-      it('Return balance and lastDeposit of account with NO funds', async () => {
-        const tx = await Bank.connect(_user1).getBalanceAndLastDeposit()
-        assert.equal(tx.balance.toString(), '0')
-        assert.equal(tx.lastDeposit.toString(), '0')
-      })
+    it('Succeed when deposit is greater than 100 wei', async () => {
+      const value = 101
+      const tx = await bank.deposit({ value })
 
-      it('Return balance and lastDeposit of account with funds', async () => {
-        await deposit(1000, { account: _user2 })
+      expect(tx).to.changeEtherBalance(
+        _deployer.address,
+        _deployerInitialEtherBalance - value
+      )
 
-        const tx = await Bank.connect(_user2).getBalanceAndLastDeposit()
-        assert.equal(tx.balance.toString(), '1000')
-        assert.equal(tx.lastDeposit.toString().indexOf('167'), 0)
-      })
+      expect(tx).to.emit('etherDeposited').withArgs(_deployer.address, value)
+    })
+  })
+
+  describe('getBalanceAndLastDeposit', async () => {
+    beforeEach(async () => {
+      await bank.deposit({ value: 1000 })
+      await bank.connect(_user1).deposit({ value: 2000 })
     })
 
-    describe('withdraw', async () => {
-      it('Revert when amount is less than 100 wei', async () => {
-        const withdrawAmount = 52
-        await expect(Bank.withdraw(withdrawAmount)).to.be.revertedWith(
-          'Minimum amount to withdraw is 100 wei'
-        )
-      })
-      it('Revert when amount is less than account balance', async () => {
-        const withdrawAmount = 100000
-        await expect(Bank.withdraw(withdrawAmount)).to.be.revertedWith(
-          'Unsufficient funds'
-        )
-      })
+    it('Return balance and lastDeposit of account with NO funds', async () => {
+      const results = await bank.connect(_user2).getBalanceAndLastDeposit()
+      assert.equal(results.balance.toString(), '0')
+      assert.equal(results.lastDeposit.toString(), '0')
+    })
 
-      it('Succeed when withdraw is 100 wei', async () => {
-        const withdrawAmount = 100
-        const tx = Bank.withdraw(withdrawAmount)
+    it('Return balance and lastDeposit of account with funds', async () => {
+      const results = await bank.connect(_user1).getBalanceAndLastDeposit()
+      assert.equal(results.balance.toString(), '2000')
+      assert.equal(results.lastDeposit.toString().indexOf('167'), 0)
+    })
+  })
 
-        expect(tx).to.changeEtherBalance(
-          _deployer.address,
-          _deployerInitialEtherBalance + withdrawAmount
-        )
+  describe('withdraw', async () => {
+    beforeEach(async () => {
+      await bank.deposit({ value: 1000 })
+      await bank.connect(_user1).deposit({ value: 2000 })
+    })
 
-        expect(tx)
-          .to.emit('etherWithdrawn')
-          .withArgs(_deployer.address, withdrawAmount)
-      })
+    it('Revert when amount is less than 100 wei', async () => {
+      const withdrawAmount = 52
+      await expect(bank.withdraw(withdrawAmount)).to.be.revertedWith(
+        'Minimum amount to withdraw is 100 wei'
+      )
+    })
 
-      it('Succeed when withdraw is greater than 100 wei', async () => {
-        const withdrawAmount = 1000
-        const tx = Bank.withdraw(withdrawAmount)
+    it('Revert when amount is less than account balance', async () => {
+      const withdrawAmount = 100000
+      await expect(bank.withdraw(withdrawAmount)).to.be.revertedWith(
+        'Unsufficient funds'
+      )
+    })
 
-        expect(tx).to.changeEtherBalance(
-          _deployer.address,
-          _deployerInitialEtherBalance + withdrawAmount
-        )
+    it('Succeed when withdraw is 100 wei', async () => {
+      const withdrawAmount = 100
+      const tx = await bank.withdraw(withdrawAmount)
 
-        expect(tx)
-          .to.emit('etherWithdrawn')
-          .withArgs(_deployer.address, withdrawAmount)
-      })
+      await tx.wait(1)
+
+      expect(tx).to.changeEtherBalance(
+        _deployer.address,
+        _deployerInitialEtherBalance + withdrawAmount
+      )
+
+      expect(tx)
+        .to.emit('etherWithdrawn')
+        .withArgs(_deployer.address, withdrawAmount)
+    })
+
+    it('Succeed when withdraw is greater than 100 wei', async () => {
+      const withdrawAmount = 101
+      const tx = bank.withdraw(withdrawAmount)
+
+      await tx.wait(1)
+
+      expect(tx).to.changeEtherBalance(
+        _deployer.address,
+        _deployerInitialEtherBalance + withdrawAmount
+      )
+
+      expect(tx)
+        .to.emit('etherWithdrawn')
+        .withArgs(_deployer.address, withdrawAmount)
     })
   })
 }
