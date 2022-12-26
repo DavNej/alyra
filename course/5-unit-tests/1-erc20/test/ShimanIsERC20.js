53bec3f67079c8e89785188b2331564ae4218c55
DavNej <david.nejar@gmail.com>
2022-12-26 12:12:11 +0100

    unit-test.erc20 | test mint

--- course/5-unit-tests/1-erc20/test/ShimanIsERC20.js
+++ course/5-unit-tests/1-erc20/test/ShimanIsERC20.js
@@ -5,31 +5,37 @@ const { assert, expect } = require('chai')
 
 if (developmentChains.includes(network.name)) {
   let Shiman
-  let _owner
-  let _notOwner
-  const _initialBalance = ethers.utils.parseEther('1000000')
+  let owner
+  let notOwner
+  const initialBalance = ethers.utils.parseEther('1000000')
 
   before(async () => {
     const accounts = await ethers.getSigners()
-    _owner = accounts[0]
-    _notOwner = accounts[1]
-    await deployments.fixture(['shiman'])
+    owner = accounts[0]
+    notOwner = accounts[1]
   })
 
   describe('Units tests for smart contract ShimanIsERC20', () => {
     describe('Constructor', () => {
-      it('Deploy contract and mint initial Balance to _owner', async () => {
+      beforeEach(async () => {
+        await deployments.fixture(['shiman'])
+        Shiman = await ethers.getContract('ShimanIsERC20')
+      })
+      it('Deploy contract and mint initial Balance to owner', async () => {
         Shiman = await ethers.getContract('ShimanIsERC20')
         assert.exists(Shiman.deployed())
 
-        let balanceOfDeployer = await Shiman.balanceOf(_owner.address)
-        expect(balanceOfDeployer).eq(_initialBalance)
+        let ownerBalance = await Shiman.balanceOf(owner.address)
+        expect(ownerBalance).eq(initialBalance)
+        /*
+        Other methods
 
-        // const INITIAL_BALANCE = '1000000000000000000000000'
-        // assert(balanceOfDeployer.toString() === INITIAL_BALANCE)
+        const INITIAL_BALANCE = '1000000000000000000000000'
+        assert(ownerBalance.toString() === INITIAL_BALANCE)
 
-        // const INITIAL_BALANCE = ethers.BigNumber.from('1000000000000000000000000')
-        // expect(balanceOfDeployer).eq(INITIAL_BALANCE)
+        const INITIAL_BALANCE = ethers.BigNumber.from('1000000000000000000000000')
+        expect(ownerBalance).eq(INITIAL_BALANCE)
+        */
       })
 
       it('contract has good name and symbol', async () => {
@@ -41,6 +47,27 @@ if (developmentChains.includes(network.name)) {
       })
     })
 
+    describe('Mint', () => {
+      beforeEach(async () => {
+        await deployments.fixture(['shiman'])
+        Shiman = await ethers.getContract('ShimanIsERC20')
+      })
+      const amountToMint = 100
+
+      it('Can mint if owner', async () => {
+        await Shiman.mint(owner.address, amountToMint)
+        const ownerBalance = await Shiman.balanceOf(owner.address)
+        const newBalance = ethers.BigNumber.from(initialBalance.toString()).add(
+          amountToMint
+        )
+        assert.equal(ownerBalance.toString(), newBalance.toString())
+      })
+
+      it('Can NOT mint if NOT owner', async () => {
+        await expect(
+          Shiman.connect(notOwner).mint(notOwner.address, amountToMint)
+        ).to.be.revertedWith('Ownable: caller is not the owner')
+      })
+    })
   })
 }
-
